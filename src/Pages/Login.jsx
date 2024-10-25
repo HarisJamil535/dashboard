7b508c57ce87edb02b03051674818c73b986c6d9
Haris Jamil <harisjamil535@gmail.com>
2024-10-25 22:31:04 +0500

    chaged bg color of login signup

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -19,10 +19,10 @@ const Login = ({ setIsLoggedIn }) => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="custom-bg-gradient dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 border-[0.5px] border-solid border-[#6e28fa] border-r-1 rounded-lg space-y-4 md:space-y-6 sm:p-8">
+          <div className="p-6 border-[0.5px] border-solid border-[#fe2dbcs] border-r-1 rounded-lg space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-[#fe2dbc] md:text-2xl dark:text-white">
               Login to your account
             </h1>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -21,10 +21,10 @@ const Signup = () => {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="custom-bg-gradient dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center border-2 px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 border-[0.5px] border-solid border-[#6e28fa] border-r-1 rounded-lg space-y-4 md:space-y-6 sm:p-8">
+          <div className="p-6 border-[0.5px] border-solid border-[#fe2dbc] border-r-1 rounded-lg space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-[#fe2dbc] md:text-2xl dark:text-white">
               Create an account
             </h1>
